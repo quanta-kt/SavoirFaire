8974f3383b168834d62f759acb0a34e2a9a46faf
Abhijit Sonar <abhijit.sonar@thinkitive.com>
2024-02-10 10:23:20 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "../pages/Login";
+function Home() {
+  return (
+    <>
+      <div className="w-full min-h-screen flex items-center justify-center">
+        <Login />
+      </div>
+    </>
+  );
+}
+
+export default Home;
