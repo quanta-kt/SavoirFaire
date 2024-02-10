af05b3b62afe368dbeb1e97bd40822dfe2c3bdde
Abhijit Sonar <abhijit.sonar@thinkitive.com>
2024-02-11 03:51:01 +0530

    remove footer

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import Footer from "./Footer";
 
 export default function AppLayout() {
   return (
     <div className="w-full h-full">
       <Navbar />
       <Outlet />
-      <Footer />
     </div>
   );
 }
